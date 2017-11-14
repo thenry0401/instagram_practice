61e59d4a6d32adda786ad8c3210bae3c05629f45
thenry <thenry0401@gmail.com>
2017-11-14 16:15:51 +0900

    created Post,Comment,Tag models

--- django_app/post/models.py
+++ django_app/post/models.py
@@ -1,17 +1,30 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
 class Post(models.Model):
-    pass
+    author = models.ForeignKey(User)
+    photo = models.ImageField(blank=True)
+    my_comment = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+    like_users = models.ManyToManyField(
+        User,
+        related_name='like_posts',
+    )
+    tags = models.ManyToManyField('Tag')
 
 
 class Comment(models.Model):
-    pass
-
-
-class PostLike(models.Model):
-    pass
+    post = models.ForeignKey(Post)
+    author = models.ForeignKey(User)
+    content = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
 
 
 class Tag(models.Model):
-    pass
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return 'Tag({})'.format(self.name)
